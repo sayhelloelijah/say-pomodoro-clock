cdf4202dd26a5d47b1a6a3ba2304436d69944d33
sayhelloelijah <say@helloelijah.com>
2019-03-29 09:27:30 -0500

    Closes #8: update JS file for based on eslint

--- dist/js/app.js
+++ dist/js/app.js
@@ -103,10 +103,9 @@ const minusBreaks = document.querySelector('#minusBreaks');
 const addBreaks = document.querySelector('#addBreaks');
 const breakNum = document.querySelector('.pomodoro__break-number');
 const startButton = document.querySelector('.pomodoro__button-primary');
+
 let count = 0;
 let number = parseInt(breakNum.innerText, 10);
-
-
 
 const hideElement = (el) => {
     // Todo: add spread operator for multiple elements
@@ -114,6 +113,66 @@ const hideElement = (el) => {
     const element = el;
     element.style.display = 'none';
 };
+
+function timeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((t / 1000) % 60);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        total: t,
+        days,
+        hours,
+        minutes,
+        seconds,
+    };
+}
+
+function runClock(id, minutes) {
+    const deadline = new Date(Date.parse(new Date()) + minutes*60*1000);
+    const clock = document.querySelector(id);
+    const timerBorder = document.querySelector('.pomodoro__timer-circle');
+    timerBorder.style.animationDuration = `${minutes * 60}s`;
+
+    function updateClock() {
+        const t = timeRemaining(deadline);
+        const minutesSpan = clock.querySelector('.pomodoro__minutes');
+        const secondsSpan = clock.querySelector('.pomodoro__seconds');
+        tickAudio.volume = 0.25;
+
+        if (!tickAudio) return;
+        tickAudio.currentTime = 0;
+        minutesSpan.innerHTML = (`0${t.minutes}`).slice(-2);
+        secondsSpan.innerHTML = (`0${t.seconds}`).slice(-2);
+        tickAudio.play();
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+            endAudio.play();
+            count += 1;
+
+            if (count == Number(breakNum.innerText)) {
+                clearInterval(timeinterval);
+            } else if ((count % 2) === 0) {
+                runClock('.pomodoro', 25);
+            } else {
+                runClock('.pomodoro', 5);
+            }
+        }
+    }
+
+    updateClock(); // run function once at first to avoid delay
+    const timeinterval = setInterval(updateClock, 1000);
+}
+
+startButton.addEventListener('click', (e) => {
+    hideElement(startButton);
+    hideElement(breakContainer);
+
+    e.preventDefault(); e.stopPropagation();
+    timerBorder.classList.add('pomodoro__timer-circle--play');
+    runClock('.pomodoro', 25);
+});
 
 minusBreaks.addEventListener('click', (e) => {
     if (number <= 0) {
@@ -131,62 +190,6 @@ addBreaks.addEventListener('click', (e) => {
     number += 1;
     breakNum.innerText = number;
     return true;
-});
-
-
-function time_remaining(endtime){
-	var t = Date.parse(endtime) - Date.parse(new Date());
-	var seconds = Math.floor( (t/1000) % 60 );
-	var minutes = Math.floor( (t/1000/60) % 60 );
-	var hours = Math.floor( (t/(1000*60*60)) % 24 );
-    var days = Math.floor( t/(1000*60*60*24) );
-	return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
-}
-
-function runClock(id, minutes) {
-    var deadline = new Date(Date.parse(new Date()) + minutes*60*1000);
-    var clock = document.querySelector(id);
-    const timerBorder = document.querySelector('.pomodoro__timer-circle');
-    timerBorder.style.animationDuration = `${minutes * 60}s`;
-
-    function updateClock() {
-        var t = time_remaining(deadline);
-        var minutesSpan = clock.querySelector('.pomodoro__minutes');
-        var secondsSpan = clock.querySelector('.pomodoro__seconds');
-        tickAudio.volume = 0.25;
-
-        if (!tickAudio) return;
-        tickAudio.currentTime = 0;
-        minutesSpan.innerHTML = (`0${t.minutes}`).slice(-2);
-        secondsSpan.innerHTML = (`0${t.seconds}`).slice(-2);
-        tickAudio.play();
-        if (t.total <= 0) {
-            clearInterval(timeinterval);
-            endAudio.play();
-            count++;
-
-            if (count == Number(breakNum.innerText)) {
-                clearInterval(timeinterval);
-                return false;
-            } else if ((count % 2) === 0) {
-                runClock('.pomodoro', 25);
-            } else {
-                runClock('.pomodoro', 5);
-            }
-        }
-    }
-
-	updateClock(); // run function once at first to avoid delay
-	var timeinterval = setInterval(updateClock,1000);
-}
-
-startButton.addEventListener('click', (e) => {
-    hideElement(startButton);
-    hideElement(breakContainer);
-
-    e.preventDefault(); e.stopPropagation();
-    timerBorder.classList.add('pomodoro__timer-circle--play');
-    runClock('.pomodoro', 25);
 });
 
 
